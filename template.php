461b3f325c81a11bbcc9925b4338a92363bdc5df
Iñaki García <inaki@txorua.com>
2014-02-03 18:28:48 +0100

    Quito una cadena de debug y vuelvo a incluir dos de los tres css del sistema

--- template.php
+++ template.php
@@ -32,8 +32,8 @@ function cmotheme_process_html(&$variables) {
 
 function cmotheme_css_alter(&$css) {
     unset($css[drupal_get_path('module','system').'/system.theme.css']);
-    unset($css[drupal_get_path('module','system').'/system.menus.css']);
-    unset($css[drupal_get_path('module','system').'/system.base.css']);
+    //unset($css[drupal_get_path('module','system').'/system.menus.css']);
+    //unset($css[drupal_get_path('module','system').'/system.base.css']);
 }
 
 
