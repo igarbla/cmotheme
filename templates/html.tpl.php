45b6ca7656285a30094a0a6c744c5048a83787a2
Iñaki García <inaki@txorua.com>
2014-03-08 22:04:23 +0100

    Añado soporte para css3 media queries para ie < 9

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -52,6 +52,7 @@
   <?php print $scripts; ?>
   <!--[if lt IE 9]>
   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
   <![endif]-->
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
